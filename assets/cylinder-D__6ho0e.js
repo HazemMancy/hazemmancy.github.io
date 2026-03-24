a46606395027e23914984e2a5e67a37e5004b43f
Hazem El Mancy <hazem.mancy@github.io>
2026-03-24 12:24:15 +0000

    Updates

---
+++ assets/cylinder-D__6ho0e.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-CZoZnQFo.js";const c=e("Cylinder",[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5v14a9 3 0 0 0 18 0V5",key:"aqi0yr"}]]);export{c as C};
