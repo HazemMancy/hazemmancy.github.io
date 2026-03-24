fcf823a0f6330792f91a9121aa2e29e11b2da2c1
Hazem El Mancy <hazem.mancy@github.io>
2026-03-24 10:22:12 +0000

    Updates

--- assets/cylinder-CMC12TKb.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index-BkSqgw6V.js";const c=e("Cylinder",[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5v14a9 3 0 0 0 18 0V5",key:"aqi0yr"}]]);export{c as C};
